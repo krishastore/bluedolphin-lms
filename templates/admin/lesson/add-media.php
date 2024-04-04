0c28ec192167b472234380d97c29db01d1a56508
ankithingarajiya <ankit@krishaweb.com>
2024-04-04 18:47:00 +0530

    Add media html

--- templates/admin/lesson/add-media.php
+++ templates/admin/lesson/add-media.php
@@ -7,4 +7,25 @@
 
 ?>
 
-<h2>Add Media</h2>
+<div class="media-type-select">
+	<label>
+		<input type="radio" name="mediatype"> Video
+	</label>
+	<label>
+		<input type="radio" name="mediatype"> text
+	</label>
+</div>
+<div class="bdlms-video-type-box">
+	<div class="bdlms-media-choose">
+		<label>Choose File</label>
+		<input type="file">
+	</div>
+	<div class="bdlms-or">
+		<span>OR</span>
+	</div>
+	<div class="bdlms-media-choose">
+		<label>Add Embed Video URL</label>
+		<input type="text" placeholder="Link">
+	</div>
+</div>
+<?php wp_editor( '', 'lesson_media', array() ); ?>
