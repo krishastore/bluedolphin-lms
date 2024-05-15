d56999aade7d113c3ca3ab499127f563eb42b9c7
dilipbheda-kwt <dilip.b@krishaweb.com>
2024-05-15 10:18:41 +0530

    Improve none content text

--- templates/frontend/content-none.php
+++ templates/frontend/content-none.php
@@ -13,6 +13,7 @@
 		<div class="bdlms-lesson-note">
 			<div class="bdlms-text-xl bdlms-p-16 bdlms-bg-gray bdlms-text-center bdlms-text-primary-dark">
 				<?php
+				if ( current_user_can( 'edit_post', get_the_ID() ) ) {
 					printf(
 						/* translators: %s is Link to new post */
 						esc_html__( 'Ready to publish your first curriculum? %s.', 'bluedolphin-lms' ),
@@ -23,7 +24,10 @@
 							esc_html__( 'Get started here', 'bluedolphin-lms' )
 						)
 					);
-					?>
+				} else {
+					esc_html_e( 'No attached curriculum was found in this course.', 'bluedolphin-lms' );
+				}
+				?>
 			</div>
 		</div>
 	</div>
