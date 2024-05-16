8d136e1ae80558af5eabde29ceb8c8a113d5f0f0
dilipbheda-kwt <dilip.b@krishaweb.com>
2024-05-16 14:25:40 +0530

    Revert inline docs

--- assets/js/src/plyr.js
+++ assets/js/src/plyr.js
@@ -1,7 +1,7 @@
 import Plyr from 'plyr';
 
 // Change the second argument to your options:
-// 	
+// https://github.com/sampotts/plyr/#options
 const player = new Plyr(".lesson-video", {
 	// debug: true,
 	tooltips: {
