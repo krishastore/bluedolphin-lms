720f349544ab9e13916abc8c37221070e7278236
Dhruvang2001 <dhruvang@krishaweb.com>
2024-07-04 12:12:51 +0530

    Fix video should play from last time frame

--- assets/js/src/plyr.js
+++ assets/js/src/plyr.js
@@ -3,7 +3,6 @@ import Plyr from 'plyr';
 // Change the second argument to your options:
 // https://github.com/sampotts/plyr/#options
 const player = new Plyr(".lesson-video", {
-	// debug: true,
 	tooltips: {
 		controls: true,
 	},
@@ -14,5 +13,24 @@ player.on('ended', function() {
 	window.location.href = nextPageLink;
 	return false;
 });
+
+var key = btoa(window.location.pathname).substring(70);
+
+// Function to save the current time
+const saveCurrentTime = () => {
+	const currentTime = player.currentTime;
+	localStorage.setItem(key, currentTime);
+  };
+  
+  // Event listener for saving time before the page unloads
+  window.addEventListener('beforeunload', saveCurrentTime);
+  
+  // Retrieve and set the current time when the page loads
+  const storedTime = localStorage.getItem(key);
+  if (storedTime) {
+	player.once('loadeddata', () => {
+		player.currentTime = parseFloat(storedTime);
+	});
+}
 // Expose player so it can be used from the console
 window.player = player;
