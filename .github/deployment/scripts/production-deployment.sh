52280c8a8754b88aca794bbb79cb5cd5ef099f28
ayushdabhi31 <ayush@krishaweb.com>
2024-12-03 15:03:28 +0530

    Update deployment script with npm and composer commands

--- .github/deployment/scripts/production-deployment.sh
+++ .github/deployment/scripts/production-deployment.sh
@@ -34,4 +34,31 @@ git checkout main
 
 # Pull the latest changes from the main branch
 git pull origin main
+
+# Check if composer.json file exists
+if [ -f "composer.json" ]; then
+    # Run composer install if composer.json exists
+    echo "composer.json and composer.lock exist"
+    composer install
+else
+    echo "composer.json not found. Skipping PHP dependency installation."
+fi
+
+# Load NVM and Node.js environment
+export NVM_DIR="\$HOME/.nvm"
+[ -s "\$NVM_DIR/nvm.sh" ] && \. "\$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "\$NVM_DIR/bash_completion" ] && \. "\$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# Check if package.json file exists
+if [ -f "package.json" ]; then
+    # Run package install if package.json exists
+    echo "package.json and package-lock.json exist"
+    npm ci
+    echo "Building the project..."
+    npm run build
+else
+    echo "package.json not found. Skipping Node.js dependency installation."
+fi
+
+echo "Deployment completed successfully."
 EOF
